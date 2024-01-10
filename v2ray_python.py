18162327b6cf2648ed696eb30abc9ddf22709820
Ersci <47410365+epg900@users.noreply.github.com>
2024-01-10 14:09:48 +0330

    Update v2ray_python.py

--- v2ray_python.py
+++ v2ray_python.py
@@ -1,5 +1,5 @@
 # @title V2Ray Server
-import json, base64, uuid, os, re, time, webbrowser
+import json, base64, uuid, os, re, time, sys, webbrowser
 from IPython.display import HTML, clear_output
 from threading import Timer
 from subprocess import Popen , PIPE
@@ -51,4 +51,10 @@ config="vmess://"+base64.b64encode(json.dumps(d).encode()).decode("utf-8")
 url = pyqrcode.create(config)
 url.svg('qrcode.svg', scale=8)
 print(url.terminal(quiet_zone=1))
-webbrowser.open('qrcode.svg')
+try:
+  webbrowser.open('qrcode.svg')
+  subprocess.call(('xdg-open','e.pishvaz.jpg'))
+except Exception as e :
+  e = sys.exc_info()
+  print(e[1])
+
