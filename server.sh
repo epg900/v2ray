cc89b88653a9cc225e2f50656dbc346a878a4e11
Ersci <47410365+epg900@users.noreply.github.com>
2025-03-24 09:25:12 +0330

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,21 +1,21 @@
 cd ~
 apt update
-apt install shadowsocks-libev
-apt install unzip
+apt install shadowsocks-libev -y
+apt install unzip -y
 apt install python3-pyqrcode -y
 apt install python3-flask -y
 cd /etc/ssh
 sed -i 's/#Port 22/Port 7077/' sshd_config
 sed -i 's/#PermitTunnel no/PermitTunnel yes/' sshd_config
 service sshd restart
-cd ~
+cd v2ray
 cp v2ray/ssconfig.json /etc/shadowsocks-libev/config.json
 python3 v2ray.py &
 git clone https://github.com/epg900/epfs2.git
-cd epfs2
-python3 server.py &
-service shadowsocks-libev.service stop
-service shadowsocks-libev.service start
+cd ~
+python3 -m http.server &
+service shadowsocks-libev stop
+service shadowsocks-libev start
 ufw allow 9910
 ufw allow 8088
 ufw allow 1080
