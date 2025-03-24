6438d56e59fec98bd809d3234c5d2b3671a99a4f
Ersci <47410365+epg900@users.noreply.github.com>
2025-03-24 08:57:05 +0330

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,9 +7,16 @@ apt install python3-flask -y
 cd /etc/ssh
 sed -i 's/#Port 22/Port 7077/' sshd_config
 sed -i 's/#PermitTunnel no/PermitTunnel yes/' sshd_config
+service sshd restart
 cd ~
 cp v2ray/ssconfig.json /etc/shadowsocks-libev/config.json
 python3 v2ray.py &
 git clone https://github.com/epg900/epfs2.git
 cd epfs2
-python3 server.py
+python3 server.py &
+service shadowsocks-libev.service stop
+service shadowsocks-libev.service start
+ufw allow 9910
+ufw allow 8088
+ufw allow 1080
+
