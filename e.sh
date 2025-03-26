2c0fd67a53c3e1ee6ca9c9d97452bfc909e6b111
epg900 <epg900@gmail.com>
2025-03-26 10:41:24 +0000

    1.0

--- e.sh
+++ e.sh
@@ -1,6 +1,8 @@
 cd /etc/ssh
 sed -i 's/#Port 22/Port 7077/' sshd_config
 sed -i 's/#PermitTunnel no/PermitTunnel yes/' sshd_config
+sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/' sshd_config
 service sshd restart
-useradd ep -s /bin/true
+useradd ep -s /bin/false
 passwd ep
+passwd root
